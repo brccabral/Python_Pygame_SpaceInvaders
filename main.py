1de57e4911526d195a4ae3c1033c3725331e8739
Bruno Cabral <cabral.brc@gmail.com>
2021-10-05 08:56:55 -0700

    close window

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800,600))
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            exit()
